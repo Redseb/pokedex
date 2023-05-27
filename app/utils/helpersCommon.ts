f68ed85af2a3849ed3e0b0d96d3aedf49e8c94fa
Miko Zyzański <mikolajzyzanski@gmail.com>
2023-05-27 18:44:38 +0200

    fix toTitleCaseFirstWord

--- app/utils/helpersCommon.ts
+++ app/utils/helpersCommon.ts
@@ -1,10 +1,10 @@
 /**
  * Pads a number with leading zeros until it has the specified number of digits.
  * @param num The number to pad.
- * @param size The total number of digits that the padded number should have. Defaults to 3.
+ * @param size The total number of digits that the padded number should have. Defaults to 4.
  * @returns The padded number as a string.
  */
-export default function padNumber(num: number, size = 3): string {
+export default function padNumber(num: number, size = 4): string {
   let padded = num.toString()
   while (padded.length < size) {
     padded = "0" + padded
@@ -19,7 +19,7 @@ export default function padNumber(num: number, size = 3): string {
  */
 export function toTitleCase(str: string): string {
   return str
-    .replace(/[^a-zA-Z ]/g, "")
+    .replace(/[^a-zA-Z ]/g, " ")
     .toLowerCase()
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
@@ -32,12 +32,10 @@ export function toTitleCase(str: string): string {
  * @returns The converted string.
  */
 export function toTitleCaseFirstWord(str: string): string {
-  return (
+  return toTitleCase(
     str
-      .replace(/[^a-zA-Z ]/g, "")
+      .replace(/[^a-zA-Z ]/g, " ")
       .toLowerCase()
-      .split(" ")[0]
-      .charAt(0)
-      .toUpperCase() + str.slice(1)
+      .split(" ")[0],
   )
 }
