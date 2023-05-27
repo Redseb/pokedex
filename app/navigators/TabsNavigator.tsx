5df3c62506a44921582aec8a3ba517d6f856205b
Miko Zyzański <mikolajzyzanski@gmail.com>
2023-05-27 18:46:42 +0200

    Make HomeScreen default route

--- app/navigators/TabsNavigator.tsx
+++ app/navigators/TabsNavigator.tsx
@@ -1,17 +1,13 @@
 import React from "react"
-import {
-  HomeScreen,
-  FavoritesScreen,
-  SearchScreen,
-} from "app/screens"
+import { HomeScreen, FavoritesScreen, SearchScreen } from "app/screens"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { IconTypes, NavigationBottomTab } from "app/components"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 
 export type TabsNavigatorParamList = {
-  Home: undefined,
-  Favorites: undefined,
-  Search: undefined,
+  Home: undefined
+  Favorites: undefined
+  Search: undefined
 }
 
 export enum TabsNames {
@@ -21,20 +17,22 @@ export enum TabsNames {
 }
 
 export const TabsIcons = {
-  Home: 'home' as  IconTypes,
-  Favorites: 'star' as IconTypes,
-  Search: 'search' as IconTypes,
+  Home: "home" as IconTypes,
+  Favorites: "star" as IconTypes,
+  Search: "search" as IconTypes,
 }
 
-export type TabsNavigatorScreenProps<T extends keyof TabsNavigatorParamList> = NativeStackScreenProps<
-TabsNavigatorParamList,
-  T
->
+export type TabsNavigatorScreenProps<T extends keyof TabsNavigatorParamList> =
+  NativeStackScreenProps<TabsNavigatorParamList, T>
 
 const Tabs = createBottomTabNavigator<TabsNavigatorParamList>()
 export const TabsNavigator = () => {
   return (
-    <Tabs.Navigator screenOptions={{ headerShown: false,  }} tabBar={props => <NavigationBottomTab {...props}/>}>
+    <Tabs.Navigator
+      screenOptions={{ headerShown: false }}
+      tabBar={(props) => <NavigationBottomTab {...props} />}
+      initialRouteName={TabsNames.Home}
+    >
       <Tabs.Screen name={TabsNames.Favorites} component={FavoritesScreen} />
       <Tabs.Screen name={TabsNames.Home} component={HomeScreen} />
       <Tabs.Screen name={TabsNames.Search} component={SearchScreen} />
