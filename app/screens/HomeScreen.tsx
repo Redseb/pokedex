989f09d1ee6786da28b3f3dca445d1712514b2ef
Miko Zyzański <mikolajzyzanski@gmail.com>
2023-05-27 18:44:25 +0200

    Add ListEmptyComponent HomeScreen

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
 import React, { FC, useEffect } from "react"
 import { observer } from "mobx-react-lite"
-import { FlatList, ViewStyle } from "react-native"
+import { ActivityIndicator, FlatList, View, ViewStyle } from "react-native"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { TabsNames, TabsNavigatorScreenProps } from "app/navigators"
 import { PokeCard, Screen } from "app/components"
 import { useStores } from "app/models"
 import Constants from "expo-constants"
-import { spacing } from "app/theme"
+import { colors, spacing } from "app/theme"
 import { Pokemon } from "app/types"
 
 interface HomeScreenProps
@@ -23,7 +23,7 @@ export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
 
   const listContainer = {
     ...$listContainer,
-    paddingVertical: Constants.statusBarHeight + spacing.md,
+    paddingVertical: Constants.statusBarHeight,
   }
 
   const renderItem = ({ item }: { item: Pokemon }) => (
@@ -42,24 +42,37 @@ export const HomeScreen: FC<HomeScreenProps> = observer(function HomeScreen() {
         contentContainerStyle={listContainer}
         onEndReachedThreshold={0.5}
         onEndReached={getPokemon}
+        ListEmptyComponent={ListEmptyComponent}
       />
     </Screen>
   )
 })
+
+const ListEmptyComponent = () => (
+  <View style={$emptyContainer}>
+    <ActivityIndicator color={colors.primary} size="large" />
+  </View>
+)
 
 const $root: ViewStyle = {
   flex: 1,
 }
 
 const $rootContentContainer: ViewStyle = {
-  flex: 1,
+  flexGrow: 1,
   height: "100%",
 }
 
 const $listContainer: ViewStyle = {
   paddingHorizontal: spacing.xxs,
+  flexGrow: 1,
 }
 
 const $pokecard: ViewStyle = {
+  flex: 1,
+}
+
+const $emptyContainer: ViewStyle = {
+  justifyContent: "center",
   flex: 1,
 }
